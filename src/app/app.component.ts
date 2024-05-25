49800dd130f6ebfff4c0c461f9f0f61cae87a91e
JenifferFaria <jenifferaparecida10@gmail.com>
2024-05-24 21:50:48 -0300

    calendariozinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, forwardRef } from '@angular/core';
+import { CalendarOptions, Calendar, EventClickArg } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg, EventDragStopArg } from '@fullcalendar/interaction';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import ptLocale from '@fullcalendar/core/locales/pt-br'; // Importa a localização em português do Brasil
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+  calendarOptions?: CalendarOptions;
+  eventsModel: any;
+  @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
+
+  ngOnInit() {
+    // necessário para carregar o pacote do calendário primeiro
+    forwardRef(() => Calendar);
+
+    this.calendarOptions = {
+      plugins: [dayGridPlugin, interactionPlugin],
+      editable: true,
+      customButtons: {
+        myCustomButton: {
+          text: 'personalizado!',
+          click: function () {
+            alert('clicou no botão personalizado!');
+          }
+        }
+      },
+      headerToolbar: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth'
+      },
+      dateClick: this.handleDateClick.bind(this),
+      eventClick: this.handleEventClick.bind(this),
+      eventDragStop: this.handleEventDragStop.bind(this),
+      locale: ptLocale // Configura a localização para português
+    };
+  }
+
+  handleDateClick(arg: DateClickArg) {
+    console.log(arg);
+  }
+
+  handleEventClick(arg: EventClickArg) {
+    console.log(arg);
+  }
+
+  handleEventDragStop(arg: EventDragStopArg) {
+    console.log(arg);
+  }
+
+  updateHeader() {
+    this.calendarOptions!.headerToolbar = {
+      left: 'prev,next myCustomButton',
+      center: 'title',
+      right: ''
+    };
+  }
+
+  updateEvents() {
+    const nowDate = new Date();
+    const yearMonth = nowDate.getUTCFullYear() + '-' + (nowDate.getUTCMonth() + 1);
+
+    this.calendarOptions!.events = [{
+      title: 'Evento Atualizado',
+      start: yearMonth + '-08',
+      end: yearMonth + '-10'
+    }];
+  }
+
 }
